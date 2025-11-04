bef3f78619de140f4cbc6dcc997883854ae26bd6
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-11-04 09:35:55 +0530

    Add project controllers and routers

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-import express from 'express';
-import 'dotenv/config'
-import cors from 'cors';
-import { clerkMiddleware } from '@clerk/express'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
-import workspaceRouter from './routes/workspaceRoute.js';
-import { protect } from './middlewares/authMiddleware.js';
+import workspaceRouter from "./routes/workspaceRoute.js";
+import { protect } from "./middlewares/authMiddleware.js";
+import projectRouter from "./routes/projectRoutes.js";
 
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
-app.get('/' , (req,res) => res.send('Server is Live'));
+app.get("/", (req, res) => res.send("Server is Live"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-// Routes 
+// Routes
 
-app.use ('/api/workspaces' , protect , workspaceRouter );
+app.use("/api/workspaces", protect, workspaceRouter);
+app.use("/api/projects", protect, projectRouter);
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT ||  5000 ;
-
-app.listen(PORT , () => console.log(`Server Running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server Running on ${PORT}`));
